56ee3bf6ee89694b85dd5e4903121d536f65c789
Tariq Ahsan <tariqahsan@yahoo.com>
2021-05-10 13:38:14 -0400

    first commit

---
+++ src/main/java/org/tahsan/web/ZIAContacts.java
@@ -0,0 +1,12 @@
+package org.tahsan.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZIAContacts {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZIAContacts.class, args);
+	}
+}
